8717213ce993a0a4dae76348c4f00424cb72863f
Andreas Ekdahl <andreas.ekdahl@inlight.se>
2023-11-15 14:35:05 +0100

    Ändrat text igen

--- app/src/main/java/se/inlight/finaappen/MainActivity.kt
+++ app/src/main/java/se/inlight/finaappen/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
+            text = "Hellååå $name!",
             modifier = modifier
     )
 }
